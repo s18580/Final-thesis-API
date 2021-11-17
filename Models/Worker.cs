3aa6731698faa3fdc3b47e44941eb5e2faf3da1c
Hubert <golab.mlody@gmail.com>
2021-11-17 22:20:41 +0100

    create Worker, Supplier, Customer, Address and Representative models

---
+++ Models/Worker.cs
@@ -0,0 +1,22 @@
+﻿using Final_thesis_api.Models.DictionaryModels;
+using System.Collections.Generic;
+
+namespace Final_thesis_api.Models
+{
+    public class Worker
+    {
+        public int IdWorker { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhonerNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string PassHash { get; set; }
+        public string Salt { get; set; }
+        public int IdWorksite { get; set; }
+
+        public Worksite Worksite { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+        public ICollection<Valuation> Valuations { get; set; }
+        public ICollection<Assignment> Assignments { get; set; }
+    }
+}
