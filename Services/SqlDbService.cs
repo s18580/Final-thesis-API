de8a0a0db07cacd45c2d39d3cc77bbda2315b047
Hubert <golab.mlody@gmail.com>
2021-11-28 15:30:48 +0100

    add connection with database

---
+++ Services/SqlDbService.cs
@@ -0,0 +1,15 @@
+﻿using Final_thesis_api.Models;
+
+namespace Final_thesis_api.Services
+{
+    public class SqlDbService : IDbService
+    {
+
+        private readonly ModelContext _context;
+
+        public SqlDbService(ModelContext context)
+        {
+            _context = context;
+        }
+    }
+}
