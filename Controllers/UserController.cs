5b1168b78a4ddcfca2da40862c67c3582d0c6032
Hubert <golab.mlody@gmail.com>
2021-12-13 22:45:44 +0100

    create UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Final_thesis_api.Models.DTOs;
+
+namespace Final_thesis_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] UserLogin userLogin)
+        {
+            // run Authorize from SimpleJwtAuthenticationHandler
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginGoogle()
+        {
+            //ToDo
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterUser()
+        {
+            //ToDo
+            return Ok();
+        }
+    }
+}
